65a7ce0a8bbd5fc53c64c1af1a9dbcc6bff9ec6d
午夜忆银 <wow0218@163.com>
2014-09-02 22:38:33 +0800

    asd

--- script/script.js
+++ script/script.js
@@ -351,7 +351,7 @@ $Tencent=window.$Tencent || {
         if(endX-_.config.homepage.startX>=75){ //向右滑动
             _.lineAnimate(cvs,index,0,30,100,"",1);
             _.playMusic(mIndex);
-            _.homepageTimer();
+            //_.homepageTimer();
         	setTimeout(function(){
         		_.lineAnimate(cvs,index,25,0,300,"Out",1);
         	},100);
@@ -361,7 +361,7 @@ $Tencent=window.$Tencent || {
         }else if(endX-_.config.homepage.startX<=-75){//向左滑动
         	_.lineAnimate(cvs,index,0,-30,100,"",1); 
         	_.playMusic(mIndex);
-        	_.homepageTimer();
+        	//_.homepageTimer();
         	setTimeout(function(){
         		_.lineAnimate(cvs,index,-30,0,300,"Out",1);
         	},100);
